224c48dd19cf2e2c2ed421e3b2a38b6027e31a1a
FlausenImKopf <gluecksfeder@mailbox.org>
2024-03-28 14:17:07 +0100

    add:zip strings solution

--- 01-zip-strings/zip-strings.js
+++ 01-zip-strings/zip-strings.js
@@ -1,3 +1,25 @@
 function zipStrings(strA, strB) {
-  // Your Code here
+  let zippedString = "";
+  let aCount = 0;
+  let bCount = 0;
+  if (strA.length === 0) {
+    zippedString = strB;
+  } else if (strB.length === 0) {
+    zippedString = strA;
+  } else {
+    while (aCount < strA.length || bCount < strB.length) {
+      if (aCount < strA.length && bCount < strB.length) {
+        zippedString += strA[aCount] + strB[bCount];
+        aCount++;
+        bCount++;
+      } else if (aCount < strA.length) {
+        zippedString += strA[aCount];
+        aCount++;
+      } else if (bCount < strB.length) {
+        zippedString += strB[bCount];
+        bCount++;
+      }
+    }
+  }
+  return zippedString;
 }
