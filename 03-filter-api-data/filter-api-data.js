1411769395d36db512b70045dd9977b8b7dfc0be
FlausenImKopf <gluecksfeder@mailbox.org>
2024-03-28 16:32:37 +0100

    add: solution to filter api data

--- 03-filter-api-data/filter-api-data.js
+++ 03-filter-api-data/filter-api-data.js
@@ -1,3 +1,17 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  let result = [];
+  for (let eachObject of apiData) {
+    let count = 0;
+    for (let eachKey of mandatoryKeys) {
+      let keyCheck = eachObject[eachKey];
+      if (keyCheck !== undefined && mandatoryKeys.length === 1) {
+        result.push(eachObject);
+      } else if (keyCheck !== undefined && count < mandatoryKeys.length - 1) {
+        count++;
+      } else if (keyCheck !== undefined && count === mandatoryKeys.length - 1) {
+        result.push(eachObject);
+      }
+    }
+  }
+  return result;
 }
